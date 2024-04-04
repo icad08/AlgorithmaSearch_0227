47a91fe7ea1dfa7fb62eededf53c21238060a737
icad08 <irsyadrm08@gmail.com>
2024-04-04 15:01:56 +0700

    cout

--- search.cpp
+++ search.cpp
@@ -6,14 +6,22 @@ int n;
 int i;
 
 void input(){
+    while (true)
+    {
     cout << "Enter the number of elements: ";
     cin >> n;
     if ((n > 0) && (n  <= 20)) 
         break;
     else
     cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
-    
+
 }
+
+cout << "\n=======================\n";
+cout << "\nEnter Array elements\n";
+cout << "\n=======================\n";
+}
+
 
 int main (){
 
